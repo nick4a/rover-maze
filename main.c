ee9304a5b046624fefab5a01283f0ea409259f98
nick4a <34343981+nick4a@users.noreply.github.com>
2017-12-07 17:57:21 -0500

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 #include "qtr_driver.c"
 
 int main() {
-  //RoverSetup
   init_motors ();
   init_QTR_driver();
   
@@ -13,38 +12,37 @@ int main() {
     
     unsigned char checkSensorValue = get_QTR_value() & 0x03;
     
-    if(checkSensorValue == 0x02){
+    if(checkSensorValue == 0x00){
+      leftmotor_forward();
+      rightmotor_reverse();
+      _delay_ms(1);
+      leftmotor_brake();
+      rightmotor_brake();
+      _delay_ms(1);  
+     }
+    else if(checkSensorValue == 0x01){
       leftmotor_forward();
       rightmotor_forward();
       _delay_ms(1);
       leftmotor_brake();
       rightmotor_brake();
       _delay_ms(1);
-    } 
-    else if(checkSensorValue == 0x00){
-      leftmotor_reverse();
-      rightmotor_forward();
-      _delay_ms(1);
-      leftmotor_brake();
-      rightmotor_brake();
-      _delay_ms(1);
+    }
+    else if(checkSensorValue == 0x02){
+        leftmotor_reverse();
+        rightmotor_forward();
+        _delay_ms(1);
+        leftmotor_brake();
+        rightmotor_brake();
+        _delay_ms(1);
     }
     else if(checkSensorValue == 0x03){
-      leftmotor_forwar();
-      rightmotor_reverse();
+      leftmotor_reverse();
+      rightmotor_forward();
       _delay_ms(2);
       leftmotor_brake();
       rightmotor_brake();
       _delay_ms(1);
     }
-    else if(checkSensorValue == 0x01){
-        leftmotor_forward();
-        rightmotor_reverse();
-        _delay_ms(2);
-        leftmotor_brake();
-        rightmotor_brake();
-        _delay_ms(1);
-  }
-  
-  return 0;
+    return 0;
 }
